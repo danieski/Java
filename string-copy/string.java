015a5b2c9b8cb162ac813d342003f03e68871d86
danieski <esquivelgarciadaniel.a@cesramonycajal.com>
2023-04-16 15:15:32 +0100

    hello

---
+++ string-copy/string.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class string here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class string
+{
+    public static void main(String[] args) {
+    
+    =
+    n';[tring nombre="Juan";
+    nombre.lenght;
+    System.out.println(nombre);
+    }
+}
