abf92c16bbb0230641f4e7a824fd3118fdc1920a
danieski <esquivelgarciadaniel.a@cesramonycajal.com>
2023-04-16 16:58:31 +0100

    Test

---
+++ POO/Coche.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class Coche here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Coche
+{
+    // instance variables - replace the example below with your own
+    private int ruedas;
+    private int peso;
+    String color;
+    /**
+     * Constructor for objects of class Coche
+     */
+    public Coche()
+    {
+        // initialise instance variables
+        ruedas = 4;
+        
+        peso=500;
+        
+        //color="verde";
+    }
+    public String dime_peso(){ //GETTER
+         return "El peso del coche es " + peso;   
+    }
+    public void establece_color(){ //SETTER
+        
+        color="azul";
+    }
+    public String dime_color(){
+        return "El color del coche es " + color;
+    }
+
+}
---
+++ POO/UsoCoche.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class UsoCoche here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class UsoCoche
+{
+    public static void main(String[] args) {
+        
+        Coche micoche=new Coche();
+        
+        micoche.establece_color();
+        System.out.println(micoche.dime_color());
+    }
+}
