cb13e7050c8471aed8afc4ce933df33a550b7963
danieski <53768766+danieski@users.noreply.github.com>
2023-04-19 09:23:20 +0100

    day3

--- POO/Uso_Empleado.java
+++ POO/Uso_Empleado.java
@@ -3,11 +3,28 @@ import java.util.*;
 public class Uso_Empleado
 {
     public static void main(String[] args) {
+        /*
         Empleado empleado1 =new Empleado("Pedro gomez", 18000, 2020, 02, 12);
         Empleado empleado2 =new Empleado("ana gomez", 28000, 1020, 03, 14);
         
+        
+        
         System.out.println("Nombre :" + empleado1.dameNombre() + "Sueldo :" + 
         empleado1.dameSueldo() + " Alta: " + empleado1.dameFechaContrato());
+    
+        empleado1.subeSueldo(5);
+        
+        System.out.println("Nombre :" + empleado1.dameNombre() + "Sueldo :" + 
+        empleado1.dameSueldo() + " Alta: " + empleado1.dameFechaContrato());
+    */
+       Empleado[] misEmpleados=new Empleado[3];
+       misEmpleados[0]=new Empleado("Paco Gomez", 9000, 1999, 12, 10);
+       misEmpleados[1]=new Empleado("Ana Gomez", 2000, 1993, 11, 12);
+       
+       for (int x=0; x<2; x++){
+           System.out.println("Nombre: " + misEmpleados[x].dameNombre());
+           
+       }
     }
 }
 
