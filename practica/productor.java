71cb23d8dd37cff1d2da569355216754c98e2349
danieski <53768766+danieski@users.noreply.github.com>
2023-05-07 14:06:52 +0100

    sabado

--- practica/productor.java
+++ practica/productor.java
@@ -1,12 +1,46 @@
 public class productor {
     public String nombre;
-    public String tipoProductor;
+    public boolean esGrande;
     public double extensionTotal;
-    //public array?? listaProductos;
+    //public String[] productos;
+    //como meto un array en un objeto
+    String[] productos = {"naranjas", "limones", "manzanas"}; 
 
+    //Constructor
+    public productor(String nombre, boolean esGrande,double extensionTotal,String[] productos){
+        this.nombre=nombre;
+        this.esGrande=esGrande;
+        this.extensionTotal=extensionTotal;
+        this.productos=productos;
+    }
+    //Objeto productor
+    productor productorCoperativa=new productor("Fernando",false,4,productos); //Creo que aqui falta la lista de productos
+    
+    //getter y setters para los atributos
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre=nombre;
+    }
+
+    public double getExtension(){
+        return  extensionTotal;
+    }
+    public void setExtension(double extensionTotal){
+        this.extensionTotal=extensionTotal;
+    }
     public String getListaProductos(){
         return "Lista productos";
     }
+    public String[] getProductos() {
+        return productos;
+    }
+    public void setProductos(String[] productos) {
+        this.productos= productos;
+    }
+    
 
     public boolean esPequenoProductor(){
         
